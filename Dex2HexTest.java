4f0adf72396a574c859770ff1a11d6620dbd33d2
tekesten33@gmail.com <ubuntu@ip-172-31-83-184.ec2.internal>
2023-11-11 12:55:12 +0000

    Test java updated

--- Dex2HexTest.java
+++ Dex2HexTest.java
@@ -1,72 +1,28 @@
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import static org.junit.Assert.assertEquals;
 
 public class Dex2HexTest {
 
     @Test
-    public void testConvertToHex() {
-        assertEquals("1", Dex2Hex.convertToHex(1));
-        assertEquals("A", Dex2Hex.convertToHex(10));
-        assertEquals("1A", Dex2Hex.convertToHex(26));
-        assertEquals("64", Dex2Hex.convertToHex(100));
-        assertEquals("7FFFFFFF", Dex2Hex.convertToHex(Integer.MAX_VALUE));
-    }
-
-    @Test
-    public void testMainWithValidInput() {
-        // Redirect standard output to capture log messages
+    public void testDex2HexConversion() {
+        // Redirect System.out to capture printed output
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
 
-        Dex2Hex.main(new String[]{"255"});
-        assertEquals("Hexadecimal representation is: FF\n", outContent.toString());
+        // Provide a sample input for testing
+        String[] args = {"255"};
+        Dex2Hex.main(args);
 
-        // Reset standard output
+        // Restore the original System.out
         System.setOut(System.out);
-    }
 
-    @Test
-    public void testMainWithInvalidInput() {
-        // Redirect standard output to capture log messages
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
+        // Get the printed output
+        String printedOutput = outContent.toString().trim();
 
-        Dex2Hex.main(new String[]{"abc"});
-        assertEquals("Invalid input. Please provide a valid integer.\n", outContent.toString());
-
-        // Reset standard output
-        System.setOut(System.out);
-    }
-
-    @Test
-    public void testMainWithNegativeInput() {
-        // Redirect standard output to capture log messages
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-
-        Dex2Hex.main(new String[]{"-5"});
-        assertEquals("Please provide a non-negative integer argument.\n", outContent.toString());
-
-        // Reset standard output
-        System.setOut(System.out);
-    }
-
-    @Test
-    public void testMainWithNoInput() {
-        // Redirect standard output to capture log messages
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-
-        Dex2Hex.main(new String[]{});
-        assertEquals("Please provide a single integer argument.\n", outContent.toString());
-
-        // Reset standard output
-        System.setOut(System.out);
+        // Verify the expected output
+        assertEquals("Converting the Decimal Value 255 to Hex...\nHexadecimal representation is: FF", printedOutput);
     }
 }
