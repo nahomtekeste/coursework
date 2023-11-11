90857e38a46b707ad21bbb3f0db9cbda8bf12034
tekesten33@gmail.com <ubuntu@ip-172-31-83-184.ec2.internal>
2023-11-11 12:57:58 +0000

    java initial code version

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -1,42 +1,57 @@
-import java.util.Scanner;
-import java.util.logging.Logger;
+ 
 
-public class Dex2Hex {
-    private static final Logger logger = Logger.getLogger(Dex2Hex.class.getName());
+import java.util.Scanner; 
 
-    private Dex2Hex() {
-        // Private constructor to hide the implicit public one
-    }
+ 
 
-    public static void main(String[] args) {
-        if (args.length != 1) {
-            logger.severe("Please provide a single integer argument.");
-            return;
-        }
+class Dex2Hex 
 
-        try {
-            int decimal = Integer.parseInt(args[0]);
-            if (decimal < 0) {
-                logger.severe("Please provide a non-negative integer argument.");
-            } else {
-                String hexadecimal = convertToHex(decimal);
-                logger.info("Hexadecimal representation is: " + hexadecimal);
-            }
-        } catch (NumberFormatException e) {
-            logger.severe("Invalid input. Please provide a valid integer.");
-        }
-    }
+{ 
 
-    public static String convertToHex(int decimal) {
-        char[] hexChars = "0123456789ABCDEF".toCharArray();
-        StringBuilder hexadecimal = new StringBuilder();
+ 
 
-        while (decimal > 0) {
-            int remainder = decimal % 16;
-            hexadecimal.insert(0, hexChars[remainder]);
-            decimal = decimal / 16;
-        }
+    public static int Arg1; 
 
-        return hexadecimal.toString();
-    }
-}
+ 
+
+    public static void main(String args[]) 
+
+    { 
+
+        Arg1 = Integer.parseInt(args[0]); 
+
+        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'}; 
+
+        int rem, num; 
+
+        num = Arg1; 
+
+        String hexadecimal=""; 
+
+        System.out.println("Converting the Decimal Value " + num + " to Hex..."); 
+
+ 
+
+        while(num != 0) 
+
+        { 
+
+            rem=num%16; 
+
+            hexadecimal= ch[rem] + hexadecimal; 
+
+            num= num/16; 
+
+        } 
+
+ 
+
+        System.out.println("Hexadecimal representation is: " + hexadecimal); 
+
+ 
+
+    } 
+
+} 
+
+ 
