28038caea9efcb0fe6f48392a01cc08ac79a2a28
tekesten33@gmail.com <ubuntu@ip-172-31-83-184.ec2.internal>
2023-11-11 13:09:52 +0000

    updated with java class

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -1,57 +1,25 @@
- 
+public class Dex2Hex {
 
-import java.util.Scanner; 
+    public static int Arg1;
 
- 
+    public static void main(String args[]) {
+        Arg1 = Integer.parseInt(args[0]);
+        String hexadecimal = convertToHex(Arg1);
+        System.out.println("Hexadecimal representation is: " + hexadecimal);
+    }
 
-class Dex2Hex 
+    public static String convertToHex(int decimal) {
+        char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        int rem;
+        int num = decimal;
+        String hexadecimal = "";
 
-{ 
+        while (num != 0) {
+            rem = num % 16;
+            hexadecimal = ch[rem] + hexadecimal;
+            num = num / 16;
+        }
 
- 
-
-    public static int Arg1; 
-
- 
-
-    public static void main(String args[]) 
-
-    { 
-
-        Arg1 = Integer.parseInt(args[0]); 
-
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'}; 
-
-        int rem, num; 
-
-        num = Arg1; 
-
-        String hexadecimal=""; 
-
-        System.out.println("Converting the Decimal Value " + num + " to Hex..."); 
-
- 
-
-        while(num != 0) 
-
-        { 
-
-            rem=num%16; 
-
-            hexadecimal= ch[rem] + hexadecimal; 
-
-            num= num/16; 
-
-        } 
-
- 
-
-        System.out.println("Hexadecimal representation is: " + hexadecimal); 
-
- 
-
-    } 
-
-} 
-
- 
+        return hexadecimal;
+    }
+}
