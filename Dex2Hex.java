24223c5be6e375624e7ad0dae8451ffa21e65769
Nahom - TG <46280184+nahomtekeste@users.noreply.github.com>
2023-11-14 14:16:10 +0000

    Update Dex2Hex.java

--- Dex2Hex.java
+++ Dex2Hex.java
@@ -1,57 +1,40 @@
- 
+import java.util.Scanner;
 
-import java.util.Scanner; 
+public class Dex2Hex {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Please provide a single integer argument.");
+            return;
+        }
 
- 
+        try {
+            int decimal = Integer.parseInt(args[0]);
+            if (decimal < 0) {
+                System.out.println("Please provide a non-negative integer argument.");
+            } else {
+                String hexadecimal = convertToHex(decimal);
+                System.out.println("Hexadecimal representation is: " + hexadecimal);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please provide a valid integer.");
+        }
+    }
 
-class Dex2Hex 
+    public static String convertToHex(int decimal) {
+        char[] hexChars = "0123456789ABCDEF".toCharArray();
+        StringBuilder hexadecimal = new StringBuilder();
 
-{ 
+        while (decimal > 0) {
+            int remainder = decimal % 16;
+            hexadecimal.insert(0, hexChars[remainder]);
+            decimal = decimal / 16;
+        }
 
- 
+        return hexadecimal.toString();
+    }
+}
 
-    public static int Arg1; 
 
- 
+// this is a test of new update
 
-    public static void main(String args[]) 
-
-    { 
-
-        Arg1 = Integer.parseInt(args[0]); 
-
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'}; 
-
-        int rem, num; 
-
-        num = Arg1; 
-
-        String hexadecimal=""; 
-
-        System.out.println("Converting the Decimal Value " + num + " to Hex..."); 
-
- 
-
-        while(num != 0) 
-
-        { 
-
-            rem=num%16; 
-
-            hexadecimal= ch[rem] + hexadecimal; 
-
-            num= num/16; 
-
-        } 
-
- 
-
-        System.out.println("Hexadecimal representation is: " + hexadecimal); 
-
- 
-
-    } 
-
-} 
-
- 
+// this is a new java update comment just for test
